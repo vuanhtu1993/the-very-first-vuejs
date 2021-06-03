e1881f0a16a13967c939170222a443a375f8a817
vuanhtu1993 <vuanhtu1993>
2021-06-03 15:03:52 +0700

    first commit

---
+++ components/LearnComponent/Child.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: undefined
+    }
+  }
+}
+</script>
